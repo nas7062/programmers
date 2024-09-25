97d5f9b8ca085f255832bc4044d23d2e4fad0cf2
김민석 <84384915+nas7062@users.noreply.github.com>
2024-09-25 14:45:35 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -0,0 +1,13 @@
+function solution(s) {
+    var answer = '';
+    if(s.length%2===1)
+        answer+=s[Math.floor(s.length/2)];
+    else
+        {
+            answer+=s[Math.floor(s.length/2)-1];
+            answer+=s[Math.floor(s.length/2)];
+            
+        }
+        
+    return answer;
+}
