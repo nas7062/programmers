5ed85896ad423fdf92605aa4cfcb100705f0f4c4
김민석 <84384915+nas7062@users.noreply.github.com>
2024-09-25 14:13:11 +0900

    Update 서울에서 김서방 찾기.js

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
@@ -1,7 +1,6 @@
 function solution(seoul) {
-    var answer = '김서방은 ';
-    
+   
      let idx =seoul.indexOf("Kim");
-    let result ="에 있다"
-    return answer+idx+result ;
+    
+    return `김서방은 ${idx}에 있다`;
 }
