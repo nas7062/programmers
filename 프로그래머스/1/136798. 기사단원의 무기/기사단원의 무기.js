34aee6c458ddd8aacbed0f7604a3ae955804c54a
김민석 <84384915+nas7062@users.noreply.github.com>
2024-09-26 14:12:47 +0900

    [level 1] Title: 기사단원의 무기, Time: 97.22 ms, Memory: 39.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
@@ -0,0 +1,22 @@
+function solution(number, limit, power) {
+    var answer = 0;
+    let arr = [];
+    for(let i =1;i<=number;i++)
+        {
+            let cnt =0;
+            for(let j =1;j*j<=i;j++)
+                {
+                    if(i%j===0)
+                        {
+                           cnt++; 
+                            if(j!==i/j)
+                                cnt++;
+                        }
+                        
+                    
+                }
+            
+            arr.push(cnt);
+        }
+    return arr.map((item)=>item > limit ? power : item).reduce((a,b)=>a+b,0);
+}
