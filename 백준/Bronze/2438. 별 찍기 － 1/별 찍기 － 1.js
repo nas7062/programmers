740063a2737c04b9fa896bba8982c135f96700f3
김민석 <84384915+nas7062@users.noreply.github.com>
2025-01-31 13:56:51 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 100 ms, Memory: 9628 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.js
@@ -0,0 +1,10 @@
+const input = require("fs").readFileSync("/dev/stdin").toString().trim("");
+
+for(let i=0;i<input;i++) {
+  let arr ="";
+  for(let j=0;j<i+1;j++) {
+    arr+="*";
+  }
+  console.log(arr);
+  arr ="";
+}
