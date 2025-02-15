6c40522d82184bf8cac2d7fd3c5b5e7f039de360
김민석 <84384915+nas7062@users.noreply.github.com>
2025-02-15 15:12:47 +0900

    [level 0] Title: 모음 제거, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.js
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.js
@@ -1,13 +1,7 @@
 function solution(my_string) {
-    let answer ="";
-    let moum = ['a','e','i','o','u'];
-     for(let str of my_string) {
-        if(moum.includes(str)) {
-            continue;
-        }
-         else {
-             answer+=str;
-        }
-     }
-    return answer;
+    let moum = ['a', 'e', 'i', 'o', 'u'];
+    for (let m of moum) {
+        my_string = my_string.replaceAll(m, ""); 
+    }
+    return my_string;
 }
