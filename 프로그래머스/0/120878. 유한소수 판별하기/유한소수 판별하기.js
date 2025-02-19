3c3f501e8451c895f7aac845385536dbb35a439e
김민석 <84384915+nas7062@users.noreply.github.com>
2025-02-19 16:12:55 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.32 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.js
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.js
@@ -1,22 +1,29 @@
 
 function gcd(a,b) {
-    if(b===0) 
-        return a;
-    else
-        return gcd(b,a%b);
-} // 외우시면 됩니다.
-function solution(a, b) {
-    let g = b/gcd(a,b);
-    while(g !==1) {
-        if(g%2 ===0) {
-            g/=2;
-        }
-        else if(g%5===0){
-            g/=5;
-        }
-        else{
-            return 2;
-        }
+    let mini =Math.min(a,b);
+    for(let i =mini;i>=2;i--) {
+        if(a%i===0 && b%i===0)
+            return i;
     }
-    return 1;  
-} //와이라노 .. 
+}
+function solution(a, b) {
+    var answer = 0;
+    let g = gcd(a,b);
+    if(g) 
+        answer =Math.floor(b/g);
+    else 
+        answer= b;
+    let result = []; 
+    let divisor = 2; 
+    while (answer >= 2) { 
+    if (answer% divisor === 0) { 
+      result.push(divisor);
+      answer = Math.floor(answer / divisor);
+    }
+    else divisor++;
+  }
+  
+  let set =  [...new Set(result)];
+  let len =result.filter((item)=> item %2===0 || item%5===0).length;
+    return len ===result.length ? 1 : 2;
+}
