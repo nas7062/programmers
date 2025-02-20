ffbc4bf7ac8a42a92e8f8dbed26b59b815f38cc4
김민석 <84384915+nas7062@users.noreply.github.com>
2025-02-20 22:43:54 +0900

    [level 1] Title: 평균 구하기, Time: 0.06 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.js
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.js
@@ -1,5 +1,4 @@
 function solution(arr) {
-    var answer = 0;
-   answer = arr.reduce((a,b)=>a+b,0);
-    return answer/arr.length;
+    var answer = arr.reduce((a,b)=>a+b,0); // reduce로 각 배열 더해줌
+    return answer/arr.length ; // arr.length만큼 나눠서 평균 구함
 }
