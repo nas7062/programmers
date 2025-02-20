289a0b32deb2a59a48245c750821d4438f8394fc
김민석 <84384915+nas7062@users.noreply.github.com>
2025-02-21 00:03:20 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
@@ -1,4 +1,4 @@
 function solution(n) {
-    var answer = 0;
-    return Math.sqrt(n)%1===0 ? Math.pow(Math.sqrt(n)+1 ,2)  :-1;
+    return  Math.sqrt(n) % 1 ? -1 : Math.pow(Math.sqrt(n)+1,2);
+    // 제곱근 %1이 1
 }
