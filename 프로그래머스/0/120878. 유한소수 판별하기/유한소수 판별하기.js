4c94d413051d3612b80019953c62d698311201b3
김민석 <84384915+nas7062@users.noreply.github.com>
2025-02-20 11:10:19 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.29 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.js
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.js
@@ -1,18 +1,18 @@
 
 function gcd(a,b) {
+    let num =1;
     let mini =Math.min(a,b);
     for(let i =mini;i>=2;i--) {
         if(a%i===0 && b%i===0)
             return i;
     }
-}
+    return num;
+} // 기약 분수를 만들기 위해 최대공약수를 만들어줌 
 function solution(a, b) {
     var answer = 0;
-    let g = gcd(a,b);
-    if(g) 
-        answer =Math.floor(b/g);
-    else 
-        answer= b;
+    let g = gcd(a,b); // 최대공약수를 가져옴 
+    answer =Math.floor(b/g);
+    
     let result = []; 
     let divisor = 2; 
     while (answer >= 2) { 
@@ -23,7 +23,7 @@ function solution(a, b) {
     else divisor++;
   }
   
-  let set =  [...new Set(result)];
+   //let set =  [...new Set(result)]; 이걸 넣으면 안된다.. 
   let len =result.filter((item)=> item %2===0 || item%5===0).length;
     return len ===result.length ? 1 : 2;
 }
