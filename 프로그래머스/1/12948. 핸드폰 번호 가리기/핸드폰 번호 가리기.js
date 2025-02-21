765655ef73836ac86aec16316d53b0c9bdd66429
김민석 <84384915+nas7062@users.noreply.github.com>
2025-02-21 15:15:13 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,6 +1,8 @@
 function solution(phone_number) {
     var answer = '';
-    var slice = phone_number.substr(phone_number.length-4);
-    var start = "*".repeat(phone_number.length-4);
-    return start+slice;
+    answer+='*'.repeat(phone_number.length-4);
+    // repeat함수는 반복하는 함수 '*'을 phone_number.length-4만큼 반복.
+    let seeNumber= [...phone_number].splice(phone_number.length-4).join('');
+    // phone_number.length-4번 부터 뒤로 쭉 자르고 문자열로 변환
+    return answer+seeNumber;
 }
