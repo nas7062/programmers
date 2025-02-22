5d525d249a92857156bccc4b4913f1601d72e46c
김민석 <84384915+nas7062@users.noreply.github.com>
2025-02-22 12:04:58 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 39.88 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -1,23 +1,13 @@
 function solution(s) {
     var answer = [];
-    var str=  "";
-    
-     for(let i =0;i<s.length;i++)
-         {
-             
-             if(!str.includes(s[i]))
-                 {
-                     str+=s[i];
-                     answer.push(-1);
-                }
-             else
-                 {
-                     let p = str.lastIndexOf(s[i]);
-                     str+=s[i];
-                     answer.push(i-p);
-                 }
-                 
-         }
-    
+    let arr= [];
+    for(let i =0;i<s.length;i++) {
+        if(arr.indexOf(s[i]) === -1) // arr에서 s[i]를 찾지 못하면 arnwer에 -1 push
+            answer.push(-1);
+        else// arr에 s[i]가 있다면 현재 index - arr에 넣어논 s[i] 마지막 인덱스를 뺴줌 
+            answer.push(i-arr.lastIndexOf(s[i])); 
+        arr.push(s[i]); // arr에 s[i] 넣어줌
+        
+    }
     return answer;
 }
