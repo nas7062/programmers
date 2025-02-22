5486743040421f2c4574b1ea8e2d15decfbe71fa
김민석 <84384915+nas7062@users.noreply.github.com>
2025-02-22 11:49:10 +0900

    [level 1] Title: 시저 암호, Time: 45.90 ms, Memory: 39.6 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.js
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.js
@@ -1,27 +1,24 @@
 function solution(s, n) {
     var answer = '';
-
-    for(let i =0;i<s.length;i++)
-        {
-            if(s[i]===" ")
-                answer+=" ";
-            else
-                {
-                    let char = s.charCodeAt(i);
-                    if(char <=90)
-                        {
-                            char+=n;
-                            if(char>90)
-                                char-=26;
-                        }
-                    else
-                    {
-                        char+=n;
-                        if(char >122)
-                           char-=26;
-                    }
-                    answer+=String.fromCharCode(char);
-                }      
+    for(let i=0;i<s.length;i++) {
+        if(s[i]===" "){
+              answer+=" ";
+            continue;
         }
+          
+        let char =s.charCodeAt(i);
+        console.log(char);
+        if( char <=90) {
+            if(char+n >90){
+                char-=26;
+            }
+        }
+        if(char <=122){
+            if(char+n >122)
+                char-=26;
+        }
+        char+=n;
+        answer+=String.fromCharCode(char);
+    };
     return answer;
 }
