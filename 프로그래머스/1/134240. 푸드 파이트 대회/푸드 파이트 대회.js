818e3e942e6102a3f55389a39d04c53276c435a0
김민석 <84384915+nas7062@users.noreply.github.com>
2025-02-22 12:29:20 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -1,8 +1,11 @@
 function solution(food) {
     var answer = '';
-    let foodarr =food.map((item)=>Math.floor(item/2));
-    
-     foodarr.map((item,idx)=>answer+=String(idx).repeat(item));
-    var last =answer.split("").reverse().join("");
-    return answer+"0"+last;
+    for(let i =0;i<food.length;i++) {
+        answer+= i.toString().repeat(Math.floor(food[i]/2));
+    } // i를 스트링으로 food[i]/2만큼 반복하여 문자열에 추가
+    answer+='0'; // 0을 문자열에 추가
+    for(let i =food.length;i>=0;i--) { // 반대로 추가 
+        answer+= i.toString().repeat(Math.floor(food[i]/2));
+    }
+    return answer;
 }
