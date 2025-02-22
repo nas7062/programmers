5efacd93e8b93362493951db3c2d359d17b44c50
김민석 <84384915+nas7062@users.noreply.github.com>
2025-02-22 21:54:08 +0900

    [level 1] Title: 콜라 문제, Time: 1.46 ms, Memory: 36.3 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.js
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.js
@@ -1,10 +1,9 @@
 function solution(a, b, n) {
     var answer = 0;
-    while(n>=a)
-        {
-            n-=a;
-            answer+=b;
-            n+=b;
-        }
+    while(n>=a) { // 콜라 빈병의 개수가 교환가능할 때 까지 
+        n-=a; // 빈병 a개를 빼고 
+        n+=b; // b개를 받아옴 
+        answer+=b; // 받아온 콜라 체크
+    }
     return answer;
 }
