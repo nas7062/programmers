04f6af4b5ee7caccc4a0b8d5d0c58e52e52470d8
김민석 <84384915+nas7062@users.noreply.github.com>
2025-02-23 20:19:32 +0900

    [level 1] Title: 과일 장수, Time: 225.19 ms, Memory: 95.1 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.js
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.js
@@ -1,15 +1,10 @@
 function solution(k, m, score) {
     var answer = 0;
-    score =score.sort((a,b)=>b-a);
-    for(let i =0;i<score.length+1;i+=m)
-        {
-            let p = score.slice(i,i+m);
-            if(p.length<m)
-                continue;
-            else
-                answer+=Math.min(...p)*m;
-            
-        }
-   
+    score=score.sort((a,b)=>a-b); //올림차순 정렬
+    while(score.length >=m) { // m보다 크거나 같을때 까지 반복
+        let slice=score.splice(-m); //큰것부터 m개만큼 자른 후 
+        let mini =Math.min(...slice); // 그것중에 최솟값을
+        answer+=mini*m; // 최솟값* m을 해서  더해줌
+    }
     return answer;
-}
+} // 내림차순 후 찾으면 시간초과 올림차순 후 뒤에서 부터 자르며 반복
