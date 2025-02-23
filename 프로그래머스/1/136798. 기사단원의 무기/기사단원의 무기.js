0ef8bd4561a6a0b0beb1c739c8b136e084d17b50
김민석 <84384915+nas7062@users.noreply.github.com>
2025-02-23 20:19:06 +0900

    [level 1] Title: 기사단원의 무기, Time: 85.04 ms, Memory: 39.8 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
@@ -1,22 +1,15 @@
 function solution(number, limit, power) {
-    var answer = 0;
-    let arr = [];
-    for(let i =1;i<=number;i++)
-        {
-            let cnt =0;
-            for(let j =1;j<=Math.sqrt(i);j++)
-                {
-                    if(i%j===0)
-                        {
-                           cnt++; 
-                            if(j!==i/j)
-                                cnt++;
-                        }
-                        
-                    
-                }
-            
-            arr.push(cnt);
+    let arr =[];
+    for(let i=1;i<=number;i++) {
+        let cnt =0;
+        for(let j=1;j<=Math.sqrt(i);j++) {
+            if(i%j === 0) { // 나눠지면 ++
+                cnt++;
+                if(i/j !== j) // 나눳을 때 몫도 ++ 
+                     cnt++;
+            }      
         }
-    return arr.map((item)=>item > limit ? power : item).reduce((a,b)=>a+b,0);
+        arr.push(cnt);
+    }
+    return arr.map((item)=> item >limit ? power : item).reduce((a,b)=>a+b,0);
 }
