01ed155e45be03fefe53fcff178fa710f490303f
김민석 <84384915+nas7062@users.noreply.github.com>
2025-02-23 21:16:31 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -1,14 +1,8 @@
 function solution(n) {
-    var answer = 0;
+    // n 을 2진수로 변환 후 나눈 후 filter를 이용해 1만 가지는 배열의 길이 알아냄
     var ntwo = n.toString(2).split("").filter((item)=>item==="1").length;
-    for(let i=n+1;i<=1000000;i++)
-        {
-            if(ntwo== i.toString(2).split("").filter((item)=>item==="1").length)
-            {
-                answer= i;
-                break;
-            }
-            
-        }
-    return answer;
+    for(let i=n+1;i<=1000000;i++){
+        if(ntwo== i.toString(2).split("").filter((item)=>item==="1").length) 
+            return i; // n이 가지는 1의 갯수와 같은 n보다 큰 숫자를 찾음 
+    }
 }
