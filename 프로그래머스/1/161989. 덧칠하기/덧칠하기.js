788f591a9f90fa1a066a104328d523d2649fc466
김민석 <84384915+nas7062@users.noreply.github.com>
2025-02-23 21:54:41 +0900

    [level 1] Title: 덧칠하기, Time: 37.09 ms, Memory: 40.8 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.js
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.js
@@ -1,23 +1,17 @@
 function solution(n, m, section) {
-    var answer = 0;
+    let arr= new Array(n).fill(1); // 
     section=section.sort((a,b)=>a-b);
-    var arr =new Array(n).fill(1);
-    for(let i =0;i<n;i++)
-        {
-            arr[section[i]-1]--;
+    let cnt=0;
+    for(let i=0;i<section.length;i++) {
+        arr[section[i]-1]--;
+    }
+    for(let i=0;i<n;i++){
+        if(arr[i] ===0) {
+        for(let j =i;j<i+m;j++) {
+            arr[j]++;
         }
-    for(let i =0;i<n;i++)
-        {
-            if(arr[i]===0)
-                {
-                    for(let j=i;j<i+m;j++)
-                        {
-                            arr[j]++;
-                        }
-                    answer++;
-                }
-              
-            
-        }
-    return answer;
+        cnt++;
+      }
+    }
+    return cnt;
 }
