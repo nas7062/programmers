bfed88a7a3d5ec5840cfc2298f2bf60bfdf246a1
김민석 <84384915+nas7062@users.noreply.github.com>
2025-02-23 20:19:14 +0900

    [level 1] Title: 2016년, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.js
+++ 프로그래머스/1/12901. 2016년/2016년.js
@@ -1,11 +1,10 @@
 function solution(a, b) {
-    var answer = '';
-    var days =["FRI","SAT","SUN","MON","TUE","WED","THU"];
-    var monthdays =[31,29,31,30,31,30,31,31,30,31,30,31];
-    var sum=b-1;
-    for(let i =0;i<a-1;i++)
-        {
-            sum+=monthdays[i];
-        }
-    return days[sum%7];
+    let days =[31,29,31,30,31,30,31,31,30,31,30,31];
+    let day =["FRI","SAT","SUN","MON","TUE","WED","THU"];
+    let sum =0;
+    for(let i=0;i<a-1;i++) { // 5월이면 4월까지 더하고
+        sum+=days[i];
+    }
+    sum+=b-1; // 나머지 일수 더함 
+    return day[sum%7];
 }
