a55766ed41a81219ed37a2064bd416182ee1ecdb
김민석 <84384915+nas7062@users.noreply.github.com>
2025-02-24 15:23:59 +0900

    [level 1] Title: 소수 찾기, Time: 53.14 ms, Memory: 45 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
@@ -1,21 +1,15 @@
 function solution(n) {
-    // index[0]이 존재하므로 배열을 num + 1로 선언
-    let arr = Array(n + 1).fill(true)
-
-        // 0과 1은 소수가 아님 => false 처리
-    arr[0] = false
-    arr[1] = false
-
+    // index[0]이 존재하므로 배열을 num + 1로 선언  0과 1은 소수가 아님 => false 처리
+    let arr = new Array(n + 1).fill(true).fill(false,0,2);
         // 제곱근까지만 반복
-    for(let i = 2; i * i <= n; i++) {
+    for(let i = 0; i <= Math.sqrt(n); i++) {
         if(arr[i]) {
             for(let j = i * i; j <= n; j += i) {
                 // 배수라면 소수가 아님 => false
-                                arr[j] = false 
+                arr[j] = false 
             }
         }
     }
-
-        // true 값만 출력
+        // true 값만 출력 
     return arr.filter(el => el).length
-}
+} 
