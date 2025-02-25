b82bf1ff1464e20d69c83b7e21bdf89aa59a5132
김민석 <84384915+nas7062@users.noreply.github.com>
2025-02-25 17:20:55 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.16 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.js
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.js
@@ -1,51 +1,32 @@
 function solution(dartResult) {
-    var answer = 0;
-    var score=[];
-    var idx =-1;
-    let number = 0;
-    for(let i =0;i<dartResult.length;i++)
-        {
-            
-            if(dartResult[i]>="0" &&dartResult[i]<="9")
-            {
-                
-                if(dartResult[i]==="1"&& dartResult[i+1]==="0")
-                    {
-                        number = 10;
-                        i++;
-                    }
-                    
-                else
-                    number = Number(dartResult[i]);
-                idx++;
-                score[idx]= number;
+    let result=[];
+    let num =0;
+    for(let i=0;i<dartResult.length;i++) {
+        let s =dartResult[i];
+        if(s>='0' && s <="9"){
+            if(s==="1" && dartResult[i+1]==="0") {
+                num=10;
+                i++;
             }
-             if(dartResult[i]==="S")
-                {
-                    score[idx] = score[idx];
-                }
-                
-             if(dartResult[i]==="D")
-                {
-                   score[idx] = Math.pow(score[idx],2);
-                }
-                
-              if(dartResult[i]==="T")
-                {
-                     score[idx] = Math.pow(score[idx],3);
-                }
-             if(dartResult[i]==="#")
-                {
-                     score[idx] = -score[idx];
-                }
-             if(dartResult[i]==="*")
-                {
-                    score[idx] =score[idx]*2;
-                    if(idx>0)
-                        score[idx-1] = score[idx-1] *2;
-                }
-             
-            console.log(answer);
+            else num=Number(s);
         }
-    return score.reduce((a,b)=>a+b,0);
+        else if(s==="S") {
+                result.push(num);
+        }
+        else if(s==="D") {
+            result.push(Math.pow(num,2));
+        }
+        else if(s==="T") {
+           result.push(Math.pow(num,3));
+        }
+        else if(s==="*") {
+                result[result.length-1] *= 2;
+                result[result.length-2] *= 2;
+        }
+        else if(s==="#") {
+            result[result.length-1] *= -1; 
+        }
+    }
+ 
+    return result.reduce((a,b)=>a+b,0);
 }
