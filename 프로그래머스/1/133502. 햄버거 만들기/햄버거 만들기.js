08e84d658fc389779f36fb10fe4a3a0c13289b52
김민석 <84384915+nas7062@users.noreply.github.com>
2025-02-25 22:58:19 +0900

    [level 1] Title: 햄버거 만들기, Time: 198.67 ms, Memory: 97.9 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.js
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.js
@@ -1,15 +1,12 @@
-function solution(ingredient) {
+function solution(ingredient) { // 1231 찾으면 
     var answer = 0;
-    var stack= [];
-    for(let i =0;i<ingredient.length;i++)
-        {
-            stack.push(ingredient[i]);
-            if(stack.slice(-4).join("")==='1231')
-            {
-                answer++;
-                stack.splice(-4);
-            }
-            
+    let stack =[];
+    for(let i =0;i<ingredient.length;i++) {
+         stack.push(ingredient[i]);
+        if(stack.slice(-4).join('') ==="1231") {
+            answer++;
+            stack.splice(-4);
         }
+    }
     return answer;
 }
