8a2de9e13efa369c4e95172272f7c19e35819a9a
김민석 <84384915+nas7062@users.noreply.github.com>
2025-02-25 21:02:59 +0900

    [level 1] Title: 대충 만든 자판, Time: 9.11 ms, Memory: 37 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.js
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.js
@@ -1,30 +1,31 @@
 function solution(keymap, targets) {
-    var answer = new Array(targets.length).fill(0);
-     targets.forEach((item,idx)=>{
-         
-          for(let i=0;i<item.length;i++)
-              {
-                  var arr= [];
-                  for(let j =0;j<keymap.length;j++)
-                      {
-                         
-                           let p =keymap[j].indexOf(item[i]);
-                          if(p!==-1)  
-                            arr.push(p+1);
-                           
-                      }
-                  if(arr.length>0)
-                    answer[idx]+=Math.min(...arr);
-                  else
-                      {
-                          answer[idx] =-1;
-                          break;
-                      }
-                      
-                 
-              }
-         
-     })
-  
+    var answer = [];
+    targets.forEach((target,idx)=> {
+        let result =0;
+        for(let t of target) {
+            let arr =[];
+            let cnt= 0;
+            keymap.forEach((item)=> {
+                let p =item.indexOf(t);
+                if(p !==-1) {
+                    arr.push(p+1);   
+                }
+                else
+                    cnt++;
+            })
+            if (cnt === keymap.length) { // 모든 keymap에서 찾을 수 없는 경우
+                result = -1;
+                break;
+            }
+
+            if(arr.length>0) {
+                let mini = Math.min(...arr);
+                result+=mini;
+            }
+        }
+        answer.push(result);
+    })
+    if(answer.length ===0)
+            return [-1];
     return answer;
 }
