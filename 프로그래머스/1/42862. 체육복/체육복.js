0e35bc86d0ae327848a85a2b834b58535be0d33d
김민석 <84384915+nas7062@users.noreply.github.com>
2025-02-25 22:34:31 +0900

    [level 1] Title: 체육복, Time: 0.25 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.js
+++ 프로그래머스/1/42862. 체육복/체육복.js
@@ -1,35 +1,27 @@
 function solution(n, lost, reserve) {
     var answer = 0;
-    var arr =new Array(n).fill(1);
-    for(let re of reserve)
-    {
-        arr[re-1]++;
+    let arr = new Array(n).fill(1);
+    for(let i =0;i<lost.length;i++) {
+        arr[lost[i]-1]--;
     }
-    for(let lo of lost)
-        {
-            arr[lo-1]--;
+    for(let i =0;i<reserve.length;i++) {
+        arr[reserve[i]-1]++;
+    }
+    for(let i =0;i<=n;i++) {
+        if(arr[i]===0) {
+            if(arr[i+1] ===2) {
+                arr[i+1]=1;
+                arr[i]=1;
+            }
+            if(arr[i+1]===1)
+                continue;
         }
-    for(let i =0;i<arr.length+1;i++)
-        {
-            if(arr[i]===0)
-                {
-                    if(arr[i+1]===2)
-                        {
-                            arr[i+1]=1;
-                            arr[i]=1;
-                        }
-                    if(arr[i+1]===1)
-                        continue;
-                }
-            if(arr[i]===2)
-                {
-                    if(arr[i+1]===0)
-                    {
-                        arr[i]=1;
-                        arr[i+1]=1;
-                    }
-                }
-            
+        if(arr[i]===2) {
+                if(arr[i+1] ===0) {
+                    arr[i+1]=1;
+                    arr[i]=1;
+                } 
+            }
         }
-    return arr.filter((item)=>item>=1).length;
+    return arr.filter((item)=>item>0).length;
 }
