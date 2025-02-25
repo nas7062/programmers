0d2b8f378534233ce535f9867c506ab425ffd672
김민석 <84384915+nas7062@users.noreply.github.com>
2025-02-25 17:35:00 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.25 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
@@ -1,18 +1,36 @@
 function solution(lottos, win_nums) {
     var answer = [];
-    let count  =0;
-    let count2 =0;
-    lottos.forEach((item)=>item ===0 ?count++ : count);
-    win_nums.forEach((item)=> {
-                for(let lot of lottos)
-                    {
-                        if(item ===lot)
-                            count2++;             
-                    }
+    let zeroCnt =0;
+    let cnt =0;
+    lottos.forEach((lotto)=> { // lotto배열 돌면서
+        for(let win of win_nums) { 
+            if(lotto === win)//맞은 로또의 개수세기
+                cnt++;
+        }
+        if(lotto===0) // lotto배열중 0인 개수 세기
+            zeroCnt++;
     })
-    let sum =count+count2;
-    
-    switch(sum)
+    let sum = cnt+zeroCnt; //  두개다 더한 값(최대 등수 확인 가능)
+    switch(sum) {
+            case 2:
+                answer.push(5)
+                break;
+            case 3:
+                answer.push(4)
+                break;
+            case 4:
+                answer.push(3)
+                break;
+            case 5:
+                answer.push(2)
+                break;
+            case 6:
+                answer.push(1)
+                break;
+            default:
+                answer.push(6)
+    }
+    switch(cnt)
         {
             case 2:
                 answer.push(5)
@@ -32,26 +50,5 @@ function solution(lottos, win_nums) {
             default:
                 answer.push(6)
         }
-    switch(count2)
-        {
-                case 2:
-                answer.push(5)
-                break;
-            case 3:
-                answer.push(4)
-                break;
-            case 4:
-                answer.push(3)
-                break;
-            case 5:
-                answer.push(2)
-                break;
-            case 6:
-                answer.push(1)
-                break;
-            default:
-                answer.push(6)
-        }
-        
     return answer;
 }
