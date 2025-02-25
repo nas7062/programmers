c634df0d0f8bb23526d554e2d1768a0337716ebe
김민석 <84384915+nas7062@users.noreply.github.com>
2025-02-25 19:59:42 +0900

    [level 1] Title: 둘만의 암호, Time: 0.33 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.js
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.js
@@ -1,21 +1,22 @@
 function solution(s, skip, index) {
-    var answer = '';
-    let skipNum =[];
-     for(let j=0;j<skip.length;j++) {
-        let num =skip.charCodeAt(j);
+    var answer = ''; // 결과 문자열 
+    let skipNum =[]; // skip할 문자 배열
+     for(let i=0;i<skip.length;i++) {
+        let num =skip.charCodeAt(i);
             skipNum.push(num);
         }
     for(let i =0;i<s.length;i++) {
-     let num= s.charCodeAt(i);
-     let idx =index;
-        while(idx--) {
-            num++;
+     let num= s.charCodeAt(i); 
+     let idx =index; 
+        while(idx--) { // 인덱스만큼 더해주기 위해 
+            num++; // num++;
             if(num >122) 
-                num=97;
-            while(skipNum.includes(num)) {
-                num++; 
+                num-=26; //z가되면 a가 되도록
+            while(skipNum.includes(num)) {  //skip숫자가 되면 한번더 num++
+                 num++;
                 if (num > 122) 
-                    num = 97;   
+                    num-=26;   //z가되면 a가 되도록
+                
             }  
         }
         answer+=String.fromCharCode(num);
