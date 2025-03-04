9f710d9a5551d46fdc49c08a40c65dfa5f5402c4
김민석 <84384915+nas7062@users.noreply.github.com>
2025-03-04 21:29:58 +0900

    [level 1] Title: 유연근무제, Time: 23.33 ms, Memory: 38 MB -BaekjoonHub

---
+++ 프로그래머스/1/388351. 유연근무제/유연근무제.js
@@ -0,0 +1,26 @@
+function solution(schedules, timelogs, startday) {
+    var answer = 0;
+    for(let i=0;i<timelogs.length;i++) {
+        let goal = schedules[i] + 10;
+        if (goal % 100 >= 60) {
+            goal += 100;
+            goal -= 60;
+        }
+        console.log(goal);
+        let cnt=0;
+         for(let j =0;j<timelogs[i].length;j++){
+            let ref = j + startday;
+            if (ref % 7 === 0 || ref % 7 === 6) {
+                continue;
+            }
+             if(goal < timelogs[i][j] ) {
+                 break;
+             }
+            cnt++;
+             
+            if(cnt===5)
+                answer++;
+        }
+    }
+    return answer;
+}
