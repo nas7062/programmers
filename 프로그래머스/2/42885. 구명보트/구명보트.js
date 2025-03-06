6f338ab9225ca4b40802ce05388239ddb8f0ec25
김민석 <84384915+nas7062@users.noreply.github.com>
2025-03-06 23:09:04 +0900

    [level 2] Title: 구명보트, Time: 35.98 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.js
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -1,17 +1,17 @@
 function solution(people, limit) {
-    var answer= 0;
-    var left = 0;
-    var right =people.length-1;
-    people = people.sort((a,b)=>a-b);
-    while(left<=right)
-        {
-        
-            if(people[left]+people[right]<=limit)
-                left++;
-            right--;
-            answer++;
-            
-        }
+    var answer = 0;
+    people=  people.sort((a,b)=>a-b);
+    let left =0;
+    let right =people.length-1;
     
+    while(left<=right) {
+        if(people[left]+people[right]<=limit) {
+           left++;
+           right--; 
+        }  
+        else
+            right--;
+        answer++;
+    }
     return answer;
 }
