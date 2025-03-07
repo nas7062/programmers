d9a5883bf23c008f2c5ef117703cfefc8cc475d6
김민석 <84384915+nas7062@users.noreply.github.com>
2025-03-07 17:10:36 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.21 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.js
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.js
@@ -1,21 +1,18 @@
-function gcd(a,b){
-    let gcd =1;
-    for(let i =1;i<=Math.min(a,b);i++)
-        {
-            if(a%i===0 && b%i===0)
-                gcd = i;
-        }
-    return gcd;
+function gcd(a,b) {
+    let num =1;
+    for(let i =Math.min(a,b);i>=2;i--) {
+        if(a%i===0 && b%i===0)
+            return i;
+    }
+    return num;
 }
-function lcm(a,b)
-{
-    return a*b /gcd(a,b);
+function lcd(a,b) {
+    return a*b /gcd(a,b) ; 
 }
 function solution(arr) {
     var answer = 1;
-    for(let i=0;i<arr.length;i++)
-        {
-            answer=  lcm(answer,arr[i]);
-        }
+    for(let i =0;i<arr.length;i++) {
+        answer=lcd(answer,arr[i]);
+    }
     return answer;
 }
