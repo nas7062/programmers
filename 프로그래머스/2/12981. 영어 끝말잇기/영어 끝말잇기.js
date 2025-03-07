51b9cbfddaaeeea9c8bb55084276ff04fe195ccc
김민석 <84384915+nas7062@users.noreply.github.com>
2025-03-07 19:03:51 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.27 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -1,15 +1,12 @@
 function solution(n, words) {
     var answer = [];
-    
-    for(let i=1;i<words.length;i++)
-        {
-            if(words[i][0] !==words[i-1].slice(-1))
-                return [i%n+1 ,Math.floor(i/n)+1]
-            else
-                {
-                    if(words.indexOf(words[i])!==i)
-                        return [i%n+1 ,Math.floor(i/n)+1]
-                }
-        }
-      return [0,0];
+    for(let i=1;i<words.length;i++) {
+        if(words[i-1].slice(-1) !== words[i][0])
+            return [i%n+1,Math.floor(i/n)+1];
+        
+        if(words.indexOf(words[i])!==i)
+            return [i%n+1 ,Math.floor(i/n)+1]
+            
+    }
+    return [0,0];
 }
