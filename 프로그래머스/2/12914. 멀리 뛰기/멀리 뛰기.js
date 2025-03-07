4be57d3c2f7e870a65606f52fe7a37b4edd7280e
김민석 <84384915+nas7062@users.noreply.github.com>
2025-03-07 14:44:54 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.24 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -0,0 +1,10 @@
+function solution(n) {
+    var answer = 0;
+    let dp = new Array(n).fill(0);
+    dp[1] =1;
+    dp[2] =2;
+    for(let i =3;i<=n;i++) {
+        dp[i] = (dp[i-2]+dp[i-1]) %1234567;
+    }
+    return dp[n];
+}
