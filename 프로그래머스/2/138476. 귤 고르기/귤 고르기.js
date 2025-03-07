e7fc008ff0d8392cef80306347ed9749a3c2f49e
김민석 <84384915+nas7062@users.noreply.github.com>
2025-03-07 12:56:59 +0900

    [level 2] Title: 귤 고르기, Time: 87.10 ms, Memory: 63.6 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
@@ -1,17 +1,15 @@
 function solution(k, tangerine) {
     var answer = 0;
-    var size = {};
+    let size={};
     tangerine.forEach((item)=>size[item] ? size[item]++ :size[item]=1);
-    var cnt = Object.values(size);
-    cnt = cnt.sort((a,b)=>b-a);
+    let cnt  =Object.values(size);
+    cnt.sort((a,b)=>b-a);
     let sum =0;
-    let count = 0;
-    for(let c of cnt)
-        {
-            sum+=c;
-            count++;
-            if(sum>=k)
-                break;
-        }
-    return count;
+    for(let c of cnt) {
+        sum+=c;
+        answer++;
+        if(sum>=k)
+            break;
+    }
+    return answer;
 }
