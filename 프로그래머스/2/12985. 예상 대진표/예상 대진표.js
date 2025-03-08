f9df6619e0f754b4579d00c1677357bb39fdbcac
김민석 <84384915+nas7062@users.noreply.github.com>
2025-03-08 22:25:22 +0900

    [level 2] Title: 예상 대진표, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
@@ -1,15 +1,10 @@
 function solution(n,a,b)
 {
     var answer = 0;
-
-    while(a!==b)
-        {
-            answer++;
-            n/=2;
-            a=Math.ceil(a/2);
-            b=Math.ceil(b/2);
-            
-
-        }
+    while(a!==b) {
+        answer++;
+        a =Math.ceil(a/2);
+        b =Math.ceil(b/2);
+    }
     return answer;
 }
