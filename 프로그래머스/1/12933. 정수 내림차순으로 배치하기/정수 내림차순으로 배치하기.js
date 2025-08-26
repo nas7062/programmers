e0b507c0a95c6ddd00dfd2fd299ae4e7c5644c65
김민석 <84384915+nas7062@users.noreply.github.com>
2025-08-26 16:00:56 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.js
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.js
@@ -1,4 +1,4 @@
 function solution(n) {
-    return Number(n.toString().split('').sort((a,b)=>b-a).join(''));
-    // n을 문자열로 바꾼 후 split sort로 내림차순 해준 다음 문자열로 변환 후 정수형으로 바꿔줌 
+    var answer = 0;
+    return Number(n.toString().split("").sort((a,b)=>a-b).reverse().join(''));
 }
