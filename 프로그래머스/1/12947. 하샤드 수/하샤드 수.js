b663a509f4e73b7d6d80be425f3b00f2a092215e
김민석 <84384915+nas7062@users.noreply.github.com>
2025-08-26 16:13:51 +0900

    [level 1] Title: 하샤드 수, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.js
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.js
@@ -1,7 +1,5 @@
 function solution(x) {
-    // 자리수를 다 더한 값
-    let sum =x.toString().split('').reduce((a,b)=>a+Number(b),0);
-    if(x%sum!==0) // 자리수를 다 더한값으로 숫자를 나눠지지 않으면 false
-        return false;
-    return true; // 나눠지면 true;
+    let sum  =0;
+    sum = x.toString().split('').reduce((a,b)=>a+ +b,0);
+    return x %sum ? false : true ;
 }
