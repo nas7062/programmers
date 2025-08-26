e57a84fd013a05489e928cf136892a4410ef8035
김민석 <84384915+nas7062@users.noreply.github.com>
2025-08-26 16:08:35 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 0.23 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
+++ 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
@@ -1,6 +1,13 @@
 function solution(s){
-    let lArr=[...s].map((item)=>item.toLowerCase());
-    let pLen = lArr.filter((item)=>item ==='p').length;
-    let yLen = lArr.filter((item)=>item ==='y').length;
-    return pLen ===yLen ? true :false;
+    let pnum = 0;
+    let ynum =0;
+    s =s.toUpperCase();
+    for(let c of s) {
+        if(c ==='Y') {
+            ynum++;
+        }
+        else if(c ==="P")
+            pnum++;
+    }
+    return ynum !== pnum ? false : true ;
 }
