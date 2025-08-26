89712d6e33d1348fa8a18c45d556a76b93d2136f
김민석 <84384915+nas7062@users.noreply.github.com>
2025-08-26 16:46:11 +0900

    [level 1] Title: 나누어 떨어지는 숫자 배열, Time: 3.88 ms, Memory: 39.4 MB -BaekjoonHub

--- 프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/README.md
+++ 프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/README.md
@@ -1,10 +1,10 @@
 # [level 1] 나누어 떨어지는 숫자 배열 - 12910 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12910?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12910) 
 
 ### 성능 요약
 
-메모리: 36.9 MB, 시간: 2.23 ms
+메모리: 39.4 MB, 시간: 3.88 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 21일 14:38:07
+2025년 08월 26일 16:46:09
 
 ### 문제 설명
 
--- 프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.js
+++ 프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.js
@@ -1,6 +1,9 @@
 function solution(arr, divisor) {
-    // arr배열중 divisor으로 딱 나누어지는 것들만 필터 
-    arr =arr.filter((item)=>item %divisor ===0);
-    return arr.length ? arr.sort((a,b)=>a-b) : [-1];
-    // arr의 길이가 0이면 [-1]반환 아니면 arr배열 정렬 후 반환
+    var answer = [];
+    for(let ar of arr) {
+        if(ar % divisor ===0) {
+            answer.push(ar);
+        }
+    }
+    return answer.length >0 ? answer.sort((a,b)=>a-b) : [-1];
 }
