8d0c91bfc7ba1b1abecbade3146cb3f5db6a399c
김민석 <84384915+nas7062@users.noreply.github.com>
2025-08-27 12:27:48 +0900

    [level 1] Title: 예산, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.js
+++ 프로그래머스/1/12982. 예산/예산.js
@@ -1,12 +1,12 @@
 function solution(d, budget) {
     var answer = 0;
+    d = d.sort((a,b)=>a-b);
     let sum =0;
-    d =d.sort((a,b)=>a-b);
-    for(let i =0;i<d.length;i++) {
-        sum+=d[i];
-        if(sum>budget)
-            continue;
+    for(let dd of d) {
+        sum+=dd;
         answer++;
+        if(sum > budget)
+            return answer -1;
     }
     return answer;
 }
