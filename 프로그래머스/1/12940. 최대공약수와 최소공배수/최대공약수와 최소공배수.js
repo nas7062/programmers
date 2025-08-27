913c670ea6dfa18909bc2cdb3a41b42713d97adc
김민석 <84384915+nas7062@users.noreply.github.com>
2025-08-27 12:16:32 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.27 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -1,16 +1,26 @@
+
 function gcd(a,b) {
-    let num =1;
-    let mini = Math.min(a,b);
-    for(let i=mini;i>=2;i--) {
-        if(a%i===0 && b%i===0)
-            return i;
+    if(a >b) {
+        for(let i =b;i>=1;i--) {
+            if(a%i===0 && b%i===0)
+                return i;
+        }
     }
-    return num;
+    else {
+         for(let i =a;i>=1;i--) {
+            if(a%i===0 && b%i===0)
+                return i;
+        }
+    }
 }
 
 function lcm(a,b) {
-    return a*b /gcd(a,b);
+    return a*b/gcd(a,b);
 }
+
 function solution(n, m) {
-    return [gcd(n,m),lcm(n,m)];
+    var answer = [];
+    let g = gcd(n,m);
+    let l = lcm(n,m);
+    return [g,l];
 }
