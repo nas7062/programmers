1052bc35ae318a0783271be0b8916822b42cbef9
김민석 <84384915+nas7062@users.noreply.github.com>
2025-08-27 10:16:13 +0900

    [level 1] Title: 내적, Time: 0.22 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.js
+++ 프로그래머스/1/70128. 내적/내적.js
@@ -1,4 +1,3 @@
 function solution(a, b) {
-    return a.map((item,idx)=>item*b[idx]).reduce((a,b)=>a+b,0);
-    // a[0] b[0] 부터 다 곱해 나온 배열은 reduce로 다 더해줌
+    return a.map((item,idx)=>item*b[idx]).reduce((a,b)=>a+ +b,0);
 }
