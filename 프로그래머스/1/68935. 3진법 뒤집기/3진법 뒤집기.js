956ff8e08740195553121f1381b4896836c84038
김민석 <84384915+nas7062@users.noreply.github.com>
2025-08-27 12:31:36 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.js
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.js
@@ -1,8 +1,5 @@
 function solution(n) {
     var answer = 0;
-        // 3진법 변환 후 나눈 후 뒤집고 다시 문자열로
-    answer =n.toString(3).split('').reverse().join('');
-    answer =parseInt(answer,3);
-    // 그 후 다시 10진법으로 
-    return answer;
+    n =  n.toString(3).split("").reverse().join('');
+    return parseInt(n,3);
 }
