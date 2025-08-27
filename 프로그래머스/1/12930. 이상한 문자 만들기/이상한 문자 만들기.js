b6301c28d4e91fc1d2fcac5c676467b797e428b6
김민석 <84384915+nas7062@users.noreply.github.com>
2025-08-27 12:43:31 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.27 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -1,5 +1,6 @@
 function solution(s) {
-    let result = "";
-    s =s.split(' ');
-    return s.map((item)=>item.split('').map((item,idx)=>idx%2===0 ? item.toUpperCase() : item.toLowerCase()).join('')).join(' ');
+    var answer = '';
+    s= s.split(' ');
+    return s.map((item)=>item.split("").map((it,idx)=>idx%2 ? it.toLowerCase() : it.toUpperCase()).join('')).join(' ')
+    return answer;
 }
