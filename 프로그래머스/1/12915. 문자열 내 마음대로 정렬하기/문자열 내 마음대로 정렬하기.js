46cb75740bc47deb55a788ef193f788b04a988b3
김민석 <84384915+nas7062@users.noreply.github.com>
2025-08-28 10:44:48 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 46.34 ms, Memory: 35.9 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -1,12 +1,12 @@
 function solution(strings, n) {
-    strings.sort((a,b)=> { // strings 배열 끼리의 비교 
-        if(a[n]>b[n]) return 1;  // 먼저 a[n] 번째 끼리의 비교  
-        if(a[n]<b[n]) return -1; 
-        if(a[n]===b[n]) { // 같다면 사전순으로 정렬
-            if(a>b) return 1;
-            if(a<b) return -1;
-         return 0;
-        }
+    const result = strings.sort((a, b) => {
+      if (a[n] === b[n]) {
+        // n번째 글자가 같으면 사전순 정렬
+        return a.localeCompare(b);
+      } else {
+        // n번째 글자를 기준으로 정렬
+        return a[n].localeCompare(b[n]);
+      }
     });
-    return strings;
+    return result;
 }
