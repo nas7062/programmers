5ed2202a35f34a29d1f5f5d73816ea3b4670c1ac
김민석 <84384915+nas7062@users.noreply.github.com>
2025-08-28 10:57:00 +0900

    [level 1] Title: 명예의 전당 (1), Time: 23.40 ms, Memory: 36.2 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -1,14 +1,13 @@
 function solution(k, score) {
     var answer = [];
-    let result = [];
-    for(let i =0;i<score.length;i++) {
-        answer.push(score[i]); // 배열에 집어넣음
-        answer.sort((a,b)=>b-a); // 넣을때 마다 내림차순 정렬
-        if(answer.length>k) { // k개가 넘으면 젤 낮은 숫자 pop
-            answer.pop();
+    let mung = [];
+    for(let sc of score) {
+        mung.push(sc);
+        mung.sort((a,b)=>a-b);
+        if(mung.length >k) {
+            mung.splice(0,1);
         }
-        result.push(answer[answer.length-1]); // 결과 배열에 제일 낮은 숫자를 넣어줌
+        answer.push(mung[0])
     }
-    
-    return result;
+    return answer;
 }
