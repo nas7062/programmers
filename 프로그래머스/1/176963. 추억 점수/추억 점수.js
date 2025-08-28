a81043d66cb05a216e32317bd767aa99666c9179
김민석 <84384915+nas7062@users.noreply.github.com>
2025-08-28 12:54:54 +0900

    [level 1] Title: 추억 점수, Time: 6.81 ms, Memory: 36.1 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.js
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.js
@@ -1,11 +1,12 @@
 function solution(name, yearning, photo) {
-    photo =photo.map((item)=> { // photo 돌면서
-        let sum =0;
-        for(let i =0;i<name.length;i++) {
-            if(item.includes(name[i])) //name을 포함하고 있으면 더해줌!
-               sum+= yearning[i];
-        }
-        return sum;
-    });
-    return photo;
+    var answer = [];
+    let result = photo.map((item)=>item.map((p)=> {
+        let idx = name.indexOf(p);
+        if(name.indexOf(p) === -1) 
+            return 0;
+        else
+            return yearning[idx];
+        
+    }).reduce((a,b)=> a+ +b,0))
+    return result;
 }
