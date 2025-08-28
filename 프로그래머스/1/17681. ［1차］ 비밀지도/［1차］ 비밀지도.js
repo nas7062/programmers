04afe56aabc3cd3baad821ac2564eb31859a7a44
김민석 <84384915+nas7062@users.noreply.github.com>
2025-08-28 13:27:22 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.21 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,18 +1,22 @@
 function solution(n, arr1, arr2) {
-    var answer = [];
-    let arr =[]
-    for(let i =0;i<arr1.length;i++) {
-        let result ='';
-        let a1 = arr1[i].toString(2);
-        let a2 = arr2[i].toString(2);
-        let sum = (Number(a1)+Number(a2)).toString();
-        if(sum.length < n) {
-            for(let j =0;j<n-sum.length;j++)
-                result+='0';
-        }
-        result+=sum;
-        console.log(result);
-        arr.push(result);
+  var answer = [];
+  arr1.forEach((str, idx) => {
+    // 1) 각 수를 2진 문자열로 만들고, 2) 길이를 n으로 0 채움
+    let num  = str.toString(2).padStart(n, '0');
+    let num2 = arr2[idx].toString(2).padStart(n, '0');
+
+    let arr = [];
+    // 3) 자리수별로 비교해서 '#' 또는 ' ' 결정
+    for (let k = 0; k < n; k++) {
+      if (num[k] === '1' || num2[k] === '1') {
+        arr.push('#');
+      } else {
+        arr.push(' ');
+      }
     }
-    return arr.map((item)=>item.split('').map((item)=>item > 0 ? '#' : ' ').join(''));
+
+    answer.push(arr.join(''));
+  });
+
+  return answer;
 }
