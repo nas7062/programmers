bff3e179ec5d5aecd787f609eec231807bcd6a29
김민석 <84384915+nas7062@users.noreply.github.com>
2025-08-29 16:28:16 +0900

    [level 1] Title: 모의고사, Time: 5.67 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,25 +1,25 @@
 function solution(answers) {
-    var answer = []; 
-    let a=0;
-    let b=0;
-    let c=0;
-    const one =[1,2,3,4,5]; // 반복 패턴 
-    const two =[2,1,2,3,2,4,2,5];// 반복 패턴
-    const three =[3,3,1,1,2,2,4,4,5,5];// 반복 패턴
-    for(let i=0;i<answers.length;i++) {
-        if(answers[i]===one[i%one.length]) 
-            a++;
-        if(answers[i]===two[i%two.length])
-            b++;
-        if(answers[i]===three[i%three.length])
-            c++;
+    var answer = [0,0,0];
+    const one =[1,2,3,4,5];
+    const two =[2,1,2,3,2,4,2,5];
+    const three=[3,3,1,1,2,2,4,4,5,5];
+    
+    for(let i =0;i<answers.length;i++) {
+        if(one[i%5] === answers[i])
+            answer[0]++;
+        if(two[i%8] === answers[i])
+            answer[1]++;
+        if(three[i%10] === answers[i])
+            answer[2]++;
+
     }
-    let maxi =Math.max(a,Math.max(b,c)); // a,b,c를 비교해 최댓값
-    if(maxi ===a) // 최댓값이랑 같다면 answer에 push
-        answer.push(1);
-    if(maxi===b)
-        answer.push(2);
-    if(maxi===c)
-        answer.push(3);
-    return answer;
+    let max= Math.max(...answer);
+    console.log(max)
+    const result =[];
+    for(let i=0;i<answer.length;i++) {
+        if(answer[i] ===max) {
+            result.push(i+1);
+        }
+    }
+    return result;
 }
