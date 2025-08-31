cbca992fedca4905c0bef48b222312a3c470234a
김민석 <84384915+nas7062@users.noreply.github.com>
2025-08-31 20:46:20 +0900

    [level 1] Title: 소수 만들기, Time: 27.71 ms, Memory: 37.5 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
@@ -1,23 +1,25 @@
-function isPrime(n) {
-    if(n <2) return false;
-    for(let i =2;i<=Math.sqrt(n);i++) {
-        if(n%i===0)
-            return false;
-    }
+function isPrime(num) {
+  if(num === 2) {
     return true;
+  }
+  for(let i = 2; i <= Math.floor(Math.sqrt(num)); i++){
+    if(num % i === 0){
+      return false; 
+    }
+  }
+  return true; 
 }
 
 function solution(nums) {
     var answer = 0;
     for(let i =0;i<nums.length-2;i++) {
-        for(let j=i+1;j<nums.length-1;j++) {
-            for(let k =j+1;k<nums.length;k++) {
-                let sum =nums[i]+nums[j]+nums[k];
+        for(let j =i+1;j<nums.length-1;j++) {
+            for (let k =j+1;k<nums.length;k++) {
+                let sum = nums[i]+nums[j]+nums[k];
                 if(isPrime(sum))
                     answer++;
             }
         }
     }
-    
     return answer;
 }
