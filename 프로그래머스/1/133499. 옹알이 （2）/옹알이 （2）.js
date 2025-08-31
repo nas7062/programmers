df1f126c06afcd9512f1d299f46ddf17eb00dc15
김민석 <84384915+nas7062@users.noreply.github.com>
2025-08-31 20:46:36 +0900

    [level 1] Title: 옹알이 (2), Time: 7.48 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.js
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.js
@@ -1,16 +1,18 @@
 function solution(babbling) {
     var answer = 0;
-    let arr= ["aya","ye","woo","ma"];
-    for(let i =0;i<babbling.length;i++) {
-        for(let j =0;j<arr.length;j++) {
-            if(babbling[i].includes(arr[j].repeat(2))) // 같은것 연속 반복 X 
-                break;
-            babbling[i] =babbling[i].split(arr[j]).join(' '); 
-            //join(' ')공백을 나눠 arr[j]로 split했을때 나눠진 양옆에 문자끼리 합쳐졌을 때 arr[j]가 될 것을 방지!
+    let arr =['aya','ye','woo','ma'];
+    for(let b of babbling) {
+        for(let ar of arr) {
+           if(b.includes(ar.repeat(2)))
+               break;
+            b =b.split(ar).join(' ');
+            console.log(b);
         }
-         if(babbling[i].split(" ").join("").length === 0) //다시 합쳤을때 길이가0이면 카운트
-            answer++;
-        
+if(b.split(" ").join("").length === 0){
+            answer ++
+        }
     }
+    
+    
     return answer;
 }
