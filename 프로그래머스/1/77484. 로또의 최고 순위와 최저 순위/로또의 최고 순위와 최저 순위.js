356f22d225f938f53dedc76afb04c855db08e700
김민석 <84384915+nas7062@users.noreply.github.com>
2025-09-01 15:58:12 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 3.46 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
@@ -1,54 +1,69 @@
 function solution(lottos, win_nums) {
     var answer = [];
-    let zeroCnt =0;
-    let cnt =0;
-    lottos.forEach((lotto)=> { // lotto배열 돌면서
-        for(let win of win_nums) { 
-            if(lotto === win)//맞은 로또의 개수세기
-                cnt++;
-        }
-        if(lotto===0) // lotto배열중 0인 개수 세기
-            zeroCnt++;
+    let zero =0;
+    let win =0;
+    lottos.forEach((lotto)=> {
+        if(lotto===0)
+            zero++;
+        if(win_nums.includes(lotto))
+            win++;
     })
-    let sum = cnt+zeroCnt; //  두개다 더한 값(최대 등수 확인 가능)
-    switch(sum) {
-            case 2:
-                answer.push(5)
-                break;
-            case 3:
-                answer.push(4)
-                break;
-            case 4:
-                answer.push(3)
-                break;
-            case 5:
-                answer.push(2)
-                break;
-            case 6:
-                answer.push(1)
-                break;
-            default:
-                answer.push(6)
-    }
-    switch(cnt)
-        {
-            case 2:
-                answer.push(5)
-                break;
-            case 3:
-                answer.push(4)
-                break;
-            case 4:
-                answer.push(3)
-                break;
-            case 5:
-                answer.push(2)
-                break;
-            case 6:
-                answer.push(1)
-                break;
-            default:
-                answer.push(6)
+    console.log(win,zero);
+    let sum = win+zero;
+    switch(win) {
+        case 6 : {
+            answer.push(1);
+            break;
         }
-    return answer;
+        case 5 : {
+            answer.push(2);
+            break;
+        }
+        case 4 : {
+            answer.push(3);
+            break;
+        }
+        case 3 : {
+            answer.push(4);
+            break;
+        }
+        case 2 : {
+            answer.push(5);
+            break;
+        }
+        default : {
+            answer.push(6);
+            break;
+        }
+            
+    }
+    switch(sum) {
+        case 6 : {
+            answer.push(1);
+            break;
+        }
+        case 5 : {
+            answer.push(2);
+            break;
+        }
+        case 4 : {
+            answer.push(3);
+            break;
+        }
+        case 3 : {
+            answer.push(4);
+            break;
+        }
+        case 2 : {
+            answer.push(5);
+            break;
+        }
+        default : {
+            answer.push(6);
+            break;
+        }
+            
+    }
+    
+    return answer.sort((a,b)=>a-b);
 }
