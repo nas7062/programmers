751595654f745dbbd16980a248a3cc340ff3c2a5
김민석 <84384915+nas7062@users.noreply.github.com>
2025-09-01 20:42:24 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 119.72 ms, Memory: 58.8 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,8 +1,11 @@
 function solution(participant, completion) {
-    participant= participant.sort(); 
-    completion = completion.sort();
-    for(let i =0;i<participant.length;i++) { // 정렬된 배열 돌면서 다르면 반환
-        if(participant[i]!==completion[i])
+    participant.sort();
+    completion.sort();
+
+    for (let i = 0; i < completion.length; i++) {
+        if (participant[i] !== completion[i]) {
             return participant[i];
+        }
     }
+    return participant[participant.length - 1]; // 다 같다면 마지막이 미완주자
 }
