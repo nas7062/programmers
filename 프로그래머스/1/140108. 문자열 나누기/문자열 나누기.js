8261b319a5e7eb58a649cb1dd30bbebfdb0e7cfa
김민석 <84384915+nas7062@users.noreply.github.com>
2025-09-01 15:58:36 +0900

    [level 1] Title: 문자열 나누기, Time: 0.74 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.js
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.js
@@ -1,20 +1,22 @@
 function solution(s) {
-    let x =s[0]; // x를 s 0번쨰로 초기화 
-    let result =[]; // 잘라낸 문자열 담아둘 배열
-    let xCnt =1; // x와 같은것을 세는 변수 1로시작
-    let cnt =0; // x와 다른것들 세는 변수
-    for(let i =1;i<=s.length*2;i++) {
-        if(x!=s[i])
-            cnt++;
+    var answer = 0;
+    let x =s[0];
+    let xcnt =0;
+    let ycnt =0;
+    for(let i =0;i<s.length;i++) {
+        if(x===s[i])
+            xcnt++;
         else
-            xCnt++;
-        if(cnt===xCnt) {
-             if (i + 1 < s.length)
-                x = s[i + 1];
-            result.push(s.slice(i-cnt-xCnt+1,i+1)); //현재에서 cnt xcnt 빼준값 +1 부터 현재 배열까지 자름
-            cnt=0;
-            xCnt=0;
+            ycnt++;
+        
+        if(xcnt === ycnt) {
+            answer++;
+            xcnt=0;
+            ycnt=0;
+            x =s[i+1];
         }
     }
-    return result.length;
+    if(xcnt !==0 || ycnt!==0)
+        answer++;
+    return answer;
 }
