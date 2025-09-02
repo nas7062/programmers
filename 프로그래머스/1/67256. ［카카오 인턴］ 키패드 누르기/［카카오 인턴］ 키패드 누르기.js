1c7f57779054a19788110a7cc3160288585f6d1b
김민석 <84384915+nas7062@users.noreply.github.com>
2025-09-02 18:06:44 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 1.13 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.js
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.js
@@ -1,49 +1,55 @@
+function getPos(n) {    
+  return [Math.floor((n - 1) / 3), (n - 1) % 3];
+}
+function dist(a, b) {       
+  return Math.abs(a[0] - b[0]) + Math.abs(a[1] - b[1]);
+}
+
 function solution(numbers, hand) {
     var answer = '';
-    let left =[1,4,7];
-    let center =[2,5,8,0];
-    let right =[3,6,9];
-    let l = 10;
-    let r = 12;
-    for(let num of numbers){
-        if(left.includes(num)) {
-            answer+="L";
-            l =num;
+    const left =[1,4,7];
+    const right =[3,6,9];
+    const mid =[2,5,8,0];
+    const pos = [10,12];
+    numbers.map((number)=> {
+        if(left.includes(number)) {
+            answer+="L"
+            pos[0] =number;
         }
-        else if(right.includes(num)) {
+        else if (right.includes(number)) {
             answer+="R";
-            r=num;
+            pos[1] =number;
         }
-        else if(center.includes(num)) {
-            if(num===0)
-               num=11;
-            let lnum = Math.abs(num-l); 
-            let rnum =Math.abs(num-r);
-            lnum=Math.floor(lnum/3)+Math.floor(lnum%3);
-            rnum = Math.floor(rnum/3)+Math.floor(rnum%3);
+        else if(mid.includes(number)) {
+            if(number ===0)
+                number =11;
+            let left =getPos(pos[0]);
+            let right = getPos(pos[1]);
+            let num  =getPos(number);
+            const leftDist  = dist(left,  num);
+            const rightDist = dist(right, num); 
             
-            if(lnum <rnum) {
-                answer+="L";
-                l=num;
-            }
-                
-            else if(lnum >rnum) {
+           if(leftDist > rightDist) {
+               answer+="R"
+               pos[1] = number;
+           }
+           else if(leftDist <rightDist){
+                answer+="L"
+               pos[0] = number;
+           }
+            else {
+                if(hand ==="right") {
                 answer+="R";
-                r=num;
+                pos[1] = number;
             }
-                
-            else{
-                if(hand==="left") {
-                    answer+="L";
-                    l=num;
-                }
-                    
-                else {
-                    answer+="R";
-                    r=num;
-               }
+            else {
+                 answer+="L";
+                 pos[0] = number;
             }
+            }
+
+               
         }
-}
+    })
     return answer;
 }
