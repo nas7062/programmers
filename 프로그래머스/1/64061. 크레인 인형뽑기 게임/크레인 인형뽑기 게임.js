0bdcf94a73b3a8b3d5940fa45613a79a2da9a606
김민석 <84384915+nas7062@users.noreply.github.com>
2025-09-02 15:18:22 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 4.76 ms, Memory: 37.4 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
@@ -1,24 +1,24 @@
 function solution(board, moves) {
     var answer = 0;
-    let map = board.map((item)=>item.slice());
-    let stack =[];
-    moves.forEach((item)=> {
-        for(let i =0;i<map.length;i++) {
-                if(map[i][item-1]===0)
-                    continue;
-                else {
-                     if(stack[stack.length-1] === map[i][item-1]) {
-                         stack.pop();
-                         answer+=2;
-                        }
-                        else 
-                            stack.push(map[i][item-1]);
-                        map[i][item-1] =0;
-                        break;  
-                    
-                }
-        
+    let result =[];
+    for(let m of moves) {
+    
+        for(let line of board){
+            if(line[m-1] !==0) {
+                result.push(line[m-1]);
+                line[m-1] = 0;
+                break;
+            }
+           
+        }
+            if (result.length >= 2 && result[result.length-1] === result[result.length-2]) {
+  answer += 2;
+  result.pop();
+  result.pop();
+}
+       
     }
-})
+ 
+     
     return answer;
 }
