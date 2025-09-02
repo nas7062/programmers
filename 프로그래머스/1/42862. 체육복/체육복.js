fe959460e1a1452c2223e5e6dae7cd162bff3b25
김민석 <84384915+nas7062@users.noreply.github.com>
2025-09-02 14:08:39 +0900

    [level 1] Title: 체육복, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.js
+++ 프로그래머스/1/42862. 체육복/체육복.js
@@ -1,28 +1,27 @@
 function solution(n, lost, reserve) {
-    var answer = 0;
-    let arr = new Array(n).fill(1);
-    for(let i =0;i<lost.length;i++) {
-        arr[lost[i]-1]--;
-    }
-    for(let i =0;i<reserve.length;i++) {
-        arr[reserve[i]-1]++;
+    
+    // 학생들이 가지고 있는 체육복 수를 모두 1로 세팅
+    const clothes = Array(n).fill(1);
+    
+    // 체육복을 도난당한 학생들의 체육복 수를 0으로
+    lost.map((lost) => {clothes[lost-1] = 0});
+    
+    // 여벌을 가지고 있는 학생들의 체육복 수 1 증가
+    reserve.map(reserve => {clothes[reserve-1] += 1});
+
+    for(let i=0; i<n; i++){
+        // 체육복이 0개인 학생이 앞사람한테 받아왔을 때 
+        if(clothes[i] === 0 && clothes[i-1] ===2){
+            clothes[i] = 1;
+            clothes[i-1] = 1;
+        }
+        // 체육복이 0개인 학생이 뒷사람한테 받아왔을 때 
+        else if(clothes[i] === 0 && clothes[i+1] === 2){
+            clothes[i] = 1;
+            clothes[i+1]=1;
+        }
     }
     
-    for(let i =0;i<n;i++) {
-        if(arr[i]===0) {
-            if(arr[i+1] ===2) {
-                arr[i+1]=1;
-                arr[i]=1;
-            }
-            if(arr[i+1]===1)
-                continue;
-        }
-        if(arr[i]===2) {
-                if(arr[i+1] ===0) {
-                    arr[i+1]=1;
-                    arr[i]=1;
-                } 
-            }
-        }
-    return arr.filter((item)=>item>0).length;
+    // 체육복이 한개 이상인 학생들의 수
+    return clothes.filter(c => c > 0).length;
 }
