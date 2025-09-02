6b29e9258e8709a668d68b152d4731020606fd6e
김민석 <84384915+nas7062@users.noreply.github.com>
2025-09-02 14:08:51 +0900

    [level 1] Title: 숫자 짝꿍, Time: 123.37 ms, Memory: 63 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.js
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.js
@@ -1,25 +1,21 @@
 function solution(X, Y) {
-    let countX = Array(10).fill(0);
-    let countY = Array(10).fill(0);
-
-    // X, Y의 각 숫자 개수를 저장
-    for (let num of X) countX[num]++;
-    for (let num of Y) countY[num]++;
-
-    let result = [];
-
-    // 공통 숫자 중 작은 개수만큼 결과에 추가
-    for (let i = 9; i >= 0; i--) {
-        let commonCount = Math.min(countX[i], countY[i]);
-        result.push(String(i).repeat(commonCount));
-    }
-
-    let answer = result.join('');
-
-    if (answer === "") return "-1"; // 공통 숫자가 없을 때
+    var answer = '';
+    let xCnt = new Array(10).fill(0);
+    let yCnt = new Array(10).fill(0);
     
-    // 모든 숫자가 0인지 확인 (내림차순을 했기때문에)
-    if (answer[0] === '0') return "0";
-
+    for(let x of X) {
+        xCnt[Number(x)]++;
+    }
+    for(let y of Y) {
+        yCnt[Number(y)]++;
+    }
+    for(let i=9;i>=0;i--) {
+       let cnt = Math.min(xCnt[i],yCnt[i]);
+        answer+= i.toString().repeat(cnt);
+    }
+    if(answer==='')
+        return "-1";
+    if(answer[0]==='0')
+        return "0";
     return answer;
 }
