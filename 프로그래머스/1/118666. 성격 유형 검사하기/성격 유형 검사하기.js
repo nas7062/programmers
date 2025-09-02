512e3be444de52e6652517d908b798e591f12ea5
김민석 <84384915+nas7062@users.noreply.github.com>
2025-09-02 16:17:44 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 1.02 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -1,24 +1,31 @@
+
 function solution(survey, choices) {
     var answer = '';
-    let types ={R:0,T:0,C:0,F:0,J:0,M:0,A:0,N:0};
-    survey.forEach((item,idx)=> {
-        let [a,b] =item.split('');
-        let num = Math.abs(choices[idx]-4);
-        if(choices[idx] >4) {
-            types[b]+=num;
+    let result = {R:0,T:0,C:0,F:0,J:0,M:0,A:0,N:0};
+    for(let i =0;i<choices.length;i++) {
+       let num =Math.abs(choices[i]-4);
+       const [leftType, rightType] = survey[i].split("");
+       if(choices[i] < 4) {
+           result[leftType]+=num;
+       }
+        else{
+           result[rightType]+=num;
         }
-        else {
-            types[a]+=num;
-        }
-    })
-    let type = Object.keys(types);
-    for(let i=0;i<type.length;i+=2) {
-        let left = types[type[i]];
-        let right = types[type[i+1]]
-        if(left>=right)
-            answer+=type[i];
-        else 
-            answer+=type[i+1];
     }
+    
+    const type = Object.keys(result);
+    
+    for(let i =0;i<type.length;i+=2) {
+        const leftType =result[type[i]];
+        const rightType = result[type[i+1]];
+    if (leftType >= rightType) {
+      // 같거나 클 때는 왼쪽 요소
+      answer += type[i];
+    } else {
+      // 작은 때는 오른쪽 요소
+      answer += type[i + 1];
+    }
+  } 
+    
     return answer;
 }
