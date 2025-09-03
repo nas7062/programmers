860bc7dbfe71029d3a49ab3622b4fa30eab641b1
김민석 <84384915+nas7062@users.noreply.github.com>
2025-09-03 15:18:25 +0900

    [level 1] Title: 달리기 경주, Time: 247.70 ms, Memory: 84.1 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
@@ -1,15 +1,16 @@
-
 function solution(players, callings) {
+    var answer = [];
     let map = new Map();
     players.forEach((player,idx)=> {
         map.set(player,idx);
     })
-        callings.map((call)=> {
-            let idx = map.get(call);
-            let front = players[idx-1];
-            [players[idx],players[idx-1]] = [players[idx-1],players[idx]];
-             map.set(call, map.get(call) - 1);
-             map.set(front, map.get(call) + 1);
-        });
+    callings.forEach((call)=> {
+        let idx = map.get(call);
+        let front = players[idx-1];
+        [players[idx],players[idx-1]] = [players[idx-1],players[idx]]
+        
+        map.set(call,map.get(call)-1);
+        map.set(front,map.get(front)+1);
+    })
     return players;
 }
