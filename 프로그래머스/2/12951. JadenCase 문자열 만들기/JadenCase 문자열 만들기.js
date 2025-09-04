2787790d1ffbec10d5351002c8ad31b16b0f77dc
김민석 <84384915+nas7062@users.noreply.github.com>
2025-09-04 22:36:07 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.30 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -1,6 +1,4 @@
 function solution(s) {
-    s=s.split(' '); 
-    return s.map((item)=>item.split('').map((item,idx)=>idx===0 
-    ? item.toUpperCase() :item.toLowerCase()).join('')).join(' ');
-    // 공백으로 나누고 나뉜 배열을 다시 문자로 나눈 후 idx ===0 이라면(첫번째 글자라면) 대문자로 나머지는 소문자로 변환 하고 문자열로 다시 합쳐줌
+    s =s.split(' ').map((item)=>item.split("").map((ch,idx)=>idx ===0 ? ch.toUpperCase() : ch.toLowerCase()).join('')).join(' ');
+    return s;
 }
