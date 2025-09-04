90055dc8253f2300c3704ab99de8d31ba2609ddc
김민석 <84384915+nas7062@users.noreply.github.com>
2025-09-04 17:54:04 +0900

    [level 1] Title: 신고 결과 받기, Time: 229.01 ms, Memory: 82.3 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.js
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.js
@@ -1,21 +1,32 @@
 function solution(id_list, report, k) {
     var answer = [];
-    let user ={};
-    let singo={};
-    id_list.forEach((item)=> {
-        user[item] =[];
-        singo[item]=0;
+    let reporter_list ={};
+    let count_list ={};
+    id_list.forEach((id)=> {
+        reporter_list[id] = [];
+        count_list[id] = 0;
     })
-    let setRp = [...new Set(report)];
-    setRp.forEach((rp)=> {
-        let [userid,singoid] =rp.split(' ');
-        user[userid].push(singoid);
-        singo[singoid]+=1;
+    
+    
+    report.forEach((rp)=> {
+        const [repoter,trol] = rp.split(' ');
+        if(reporter_list[repoter].includes(trol) === false) {
+            reporter_list[repoter].push(trol);
+        }
+        
     })
-    for (const key in user) { // user객체 돌면서 
+     for (const key in reporter_list) {
+        for (const trol of reporter_list[key]) {
+            count_list[trol] += 1;
+        }
+    }
+
+  
+    
+    for (const key in reporter_list) {
         var count = 0;
-        for (const report_id of user[key]) { //각 키의 값들을 돌아 
-            if (singo[report_id] >= k) { // 그 키 값을 singo에 넣어 k보다 큰게 있다면 
+        for (const trol of reporter_list[key]) {
+            if (count_list[trol] >= k) {
                 count++;
             }
         }
