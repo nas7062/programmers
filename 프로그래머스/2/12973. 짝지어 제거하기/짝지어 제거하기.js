4314b75fb873638c4f9ff2765e02296fefec9e78
김민석 <84384915+nas7062@users.noreply.github.com>
2025-09-05 14:44:45 +0900

    [level 2] Title: 짝지어 제거하기, Time: 66.45 ms, Memory: 55.8 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -1,14 +1,18 @@
 function solution(s)
 {
-    var stack =[];
-   for(var i=0;i<s.length;i++){
-        if(stack.length === 0) {
-            stack.push(s[i]);
-        }else if(stack[stack.length-1] === s[i]){
-            stack.pop();
-        } else {
-            stack.push(s[i]);
+    var answer = -1;
+    let arr =[];
+    s= s.split('');
+    while(s.length) {
+        if(s[s.length-1] === s[s.length-2]) {
+            s.pop();
+            s.pop();
+        }
+        arr.push(s.pop());
+        if(arr[arr.length-1] === arr[arr.length-2]) {
+            arr.pop();
+            arr.pop();
         }
     }
-    return stack.length===0 ? 1 : 0;
+    return arr.length ===0 ? 1 : 0;
 }
