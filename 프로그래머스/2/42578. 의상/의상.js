00aecdd380af9df649edf02b3557533c94fec3e2
김민석 <84384915+nas7062@users.noreply.github.com>
2025-09-08 17:45:22 +0900

    [level 2] Title: 의상, Time: 0.75 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,16 +1,19 @@
 function solution(clothes) {
     var answer = 1;
-    let cloth ={};
-    clothes.forEach((c)=> {
-        const [name,cate] =c;
-        if(cloth.hasOwnProperty(cate)) {
-            cloth[cate]+=1;
+    let map = new Map();
+    clothes.forEach((clothe)=> {
+        if(map.get(clothe[1])) {
+            map.set(clothe[1], (map.get(clothe[1]) ?? 0) +1 ) 
         }
-        else
-            cloth[cate]= 1;
-})
-    for(let key in cloth) {
-        answer*=(cloth[key]+1);
+        else {
+            map.set(clothe[1],1);
+        }
+        
+    })
+     // 모든 경우의 수를 구해야 하므로 +1을 하여 곱해준다. 안고를 경우도 추가해서 각각 +1 하는것.
+    for (const [key, value] of map) {
+        answer *= (value + 1)
     }
-    return answer-1;
+     // 아무것도 착용하지 않는 경우를 -1을 해준다.
+     return answer - 1; 
 }
