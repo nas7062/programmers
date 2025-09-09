e31138fe21ac6ec037dbf9e591125404c42289b3
김민석 <84384915+nas7062@users.noreply.github.com>
2025-09-09 14:26:29 +0900

    [level 2] Title: 전화번호 목록, Time: 139.76 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,9 +1,11 @@
 function solution(phone_book) {
-    var answer = true;
-    phone_book.sort();
-     // 하나라도 틀리면 false (some함수) 
-   const result =  phone_book.some((book,idx)=> {
-         return phone_book[idx+1]?.startsWith(book);
-    })
-    return !result; // 여기서는 하나라도 존재한다면 true로 아니면 false 
+  phone_book.sort(); 
+
+  for (let i = 0; i < phone_book.length - 1; i++) {
+    if (phone_book[i] === phone_book[i + 1].substring(0, phone_book[i].length)) {
+      return false;
+    }
+  }
+
+  return true;
 }
