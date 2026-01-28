14b865bbc5b4e194a28c9614fa628f7c1e3c141f
김민석 <84384915+nas7062@users.noreply.github.com>
2026-01-28 13:58:19 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,5 +1,5 @@
 function solution(numbers) {
     var answer = 45;
-    let num = numbers.reduce((a,b)=>a+ +b,0);
-    return answer - num;
+    let nums = numbers.reduce((a,b)=> a+ +b,0);
+    return answer-nums;
 }
