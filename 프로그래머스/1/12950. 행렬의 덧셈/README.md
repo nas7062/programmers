637b94dd2ee58cf45612d5b5a9c8d223994a7a48
김민석 <84384915+nas7062@users.noreply.github.com>
2026-01-28 14:57:13 +0900

    [level 1] Title: 행렬의 덧셈, Time: 8.46 ms, Memory: 67.8 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/README.md
+++ 프로그래머스/1/12950. 행렬의 덧셈/README.md
@@ -1,10 +1,10 @@
 # [level 1] 행렬의 덧셈 - 12950 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12950?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12950) 
 
 ### 성능 요약
 
-메모리: 67.6 MB, 시간: 8.05 ms
+메모리: 67.8 MB, 시간: 8.46 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 27일 12:06:02
+2026년 01월 28일 14:57:12
 
 ### 문제 설명
 
--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -1,13 +1,12 @@
 function solution(arr1, arr2) {
     var answer = [];
+    
     for(let i =0;i<arr1.length;i++) {
         let arr =[];
-        let sum =0;
-        for(let j =0; j<arr1[i].length;j++) {
-            sum = arr1[i][j]+ arr2[i][j];
-            arr.push(sum);
+        for(let j =0;j<arr1[0].length;j++) {
+            arr.push(arr1[i][j]+arr2[i][j]);
         }
-        answer.push(arr)
+        answer.push(arr);
     }
     return answer;
 }
