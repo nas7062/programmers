17cb55c5daced578b63aa78cb0f8d80f4de780ad
김민석 <84384915+nas7062@users.noreply.github.com>
2026-01-28 14:53:29 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -1,10 +1,13 @@
 function solution(s) {
-  var answer;
-  if (s.length === 4 || s.length === 6) {
-    for (let i = 0; i < s.length; i++) {
-      if (isNaN(s[i])) return false;
-    }
-    return true;
-  }
-  return false;
+    var answer = true;
+    let len = s.length;
+  
+    if(len ===4 || len ===6) answer= true;
+    else answer= false;
+     [...s].forEach((ar)=> {
+         if(Number.isNaN(Number(ar)) === true) 
+             answer=false;
+        
+    })
+    return answer;
 }
