2abf0519c46d1ea43a37ce22fac22bc8ef4abe4e
김민석 <84384915+nas7062@users.noreply.github.com>
2026-01-28 13:41:49 +0900

    [level 1] Title: 하샤드 수, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.js
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.js
@@ -1,5 +1,4 @@
 function solution(x) {
-    let sum  =0;
-    sum = x.toString().split('').reduce((a,b)=>a+ +b,0);
-    return x %sum ? false : true ;
+    const num = x.toString().split('').reduce((a,b)=>a + +b,0);
+    return x% num ? false : true;
 }
