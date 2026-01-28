b2a4c7387cf78c70941e5857cd46b44f00b95ffa
김민석 <84384915+nas7062@users.noreply.github.com>
2026-01-28 14:07:51 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -1,13 +1,16 @@
 function solution(num) {
     var answer = 0;
-    while(num!==1) {
-        if(num%2===0) {
-            num/=2;
-        }
-        else {
-            num =num*3 +1;
-        }
+    let n = num;    
+
+    while(n!==1) {
+        if(answer >500) 
+            return -1;
+        if(n%2) 
+            n = n*3 +1;
+        else 
+            n/=2;
         answer++;
+        
     }
-    return answer >= 500 ? -1 : answer;
+    return answer;
 }
