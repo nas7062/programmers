602685f55af06f9af18a3f2bcc264a6e82cb9ef1
김민석 <84384915+nas7062@users.noreply.github.com>
2026-01-29 21:59:49 +0900

    [level 1] Title: 시저 암호, Time: 3.82 ms, Memory: 37.1 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.js
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.js
@@ -1,21 +1,22 @@
 function solution(s, n) {
     var answer = '';
-    for(let i =0;i<s.length;i++) {
-           if(s[i]=== " ") {
-               answer+=' '
-               continue;
-           }
-    let code =s[i].charCodeAt();
-    // 대문자 처리
-    if (code >= 65 && code <= 90) {
-      code = ((code - 65 + n) % 26) + 65;
-    }
-    // 소문자 처리
-    else if (code >= 97 && code <= 122) {
-      code = ((code - 97 + n) % 26) + 97;
-    }
-        
-    answer+=String.fromCharCode(code);
-    }
+    for(let i=0;i<s.length;i++) {
+        if(s[i]===" "){ // 공백은 answer에 공백을 더해준 후 통과
+              answer+=" ";
+            continue;
+        }
+        let char =s.charCodeAt(i); // 문자를 아스키 코드로 변환
+        if( char <=90) { // 'Z'가 90이므로  90보다 작을경우 
+            if(char+n >90){ // 근데 +n만큼 더햇는데 90보다 커진다 그럼 -26
+                char-=26;
+            }
+        }
+        if(char <=122){ // 'z'가 122
+            if(char+n >122)
+                char-=26;
+        }
+        char+=n; //char에 n만큼 더해줌
+        answer+=String.fromCharCode(char); // 숫자를 문자로 바꿔줌 
+    };
     return answer;
 }
