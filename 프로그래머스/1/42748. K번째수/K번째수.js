1e6285753359bee045d6c728d972066808bac2c4
김민석 <84384915+nas7062@users.noreply.github.com>
2026-01-31 13:58:57 +0900

    [level 1] Title: K번째수, Time: 0.10 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,8 +1,8 @@
 function solution(array, commands) {
     var answer = [];
-    commands.forEach((command)=> {
-        const str = array.slice(command[0]-1,command[1]).sort((a,b)=>a-b);
-        answer.push(str[command[2]-1]);
-    })
+    for(let i =0;i<commands.length;i++) {
+          let arr = array.slice(commands[i][0]-1,commands[i][1]).sort((a,b)=>a-b);
+          answer.push(arr[commands[i][2] -1]);
+    }
     return answer;
 }
