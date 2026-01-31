424e2a7aec8bab9625d757ee1f0687e84bda0b49
김민석 <84384915+nas7062@users.noreply.github.com>
2026-01-31 13:31:58 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,11 +1,8 @@
 function solution(s) {
-    let numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
-    var answer = s;
-
-    for(let i=0; i< numbers.length; i++) {
-        let arr = answer.split(numbers[i]);
-        answer = arr.join(i);
+    var answer = 0;
+    const numbers = ['zero','one','two','three','four','five','six','seven','eight','nine'];
+    for(let i=0; i<numbers.length;i++) {
+        s = s.split(numbers[i]).join(i);
     }
-
-    return Number(answer);
+    return +s;
 }
